7ef1b0c67181223eb50d3d79fcb1decf9278c2c4
seba <seig.astorga@gmail.com>
2019-11-22 20:07:47 -0300

    seba

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,130 +20,59 @@
                                     <p class="text-muted mb-4">No tienes una cuenta? Crea una, tomará menos de un minuto </p>
                                 </div>
 
-                                <form method="POST" action="{{ route('register') }}">
-                                    @csrf
-
-
-                                    <div class="form-group">
-<<<<<<< HEAD
-                                        <label for="fullname">{{ __('Nombre') }}</label>
-                                        <input id="name" type="text" placeholder="Ingresa tu nombre" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                                         @error('name')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="emailaddress">{{ __('Direccion de E-mail') }}</label>
-                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                        @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-=======
-                                        <label for="fullname">Nombre Completo</label>
-                                        <input class="form-control" type="text" id="fullname" placeholder="Ingresa tu nombre" required="">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="emailaddress">Dirección de Correo</label>
-                                        <input class="form-control" type="email" id="emailaddress" required="" placeholder="Ingresa un correo electrónico">
->>>>>>> seba
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="password">Contraseña</label>
-                                        <input class="form-control" type="password" required="" id="password" placeholder="Ingresa una contraseña">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" class="custom-control-input" id="checkbox-signup">
-                                            <label class="custom-control-label" for="checkbox-signup">Acepto <a href="#" class="text-muted">Terminos y Condiciones</a></label>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group mb-0 text-center">
-                                        <button class="btn btn-primary" type="submit"> Registrate </button>
-                                    </div>
-
-                                </form>
                      <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        <div class="form-group">
+                            <label for="name">{{ __('Nombre') }}</label>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" placeholder="Ingresa tu nombre" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Direccion de E-mail') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        <div class="form-group">
+                            <label for="email">{{ __('Direccion de E-mail') }}</label>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Ingresa un E-mail" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
+                    
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="form-group">
+                            <label for="password">{{ __('Contraseña') }}</label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Ingresa una contraseña" name="password" required autocomplete="new-password">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
+                                @error('Contraseña')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirma Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        <div class="form-group">
+                            <label for="password-confirm">{{ __('Confirma Contraseña') }}</label>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Repite la contraseña" required autocomplete="new-password">
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="form-group text-center mb-0">
                                 <button type="submit" class="btn btn-success">
                                     <i class="fas fa-user-plus"></i>
-                                    {{ __('Registrar') }}
+                                    {{ __('Registrate') }}
                                 </button>
-                            </div>
                         </div>
                     </form>
 
                             </div> <!-- end card-body -->
                         </div>
                         <!-- end card -->
-
-                        <div class="row mt-3">
-                            <div class="col-12 text-center">
-                                <p class="text-muted">Ya tienes una cuenta? <a href="pages-login.html" class="text-muted ml-1"><b>Inicia Sesión</b></a></p>
-                            </div> <!-- end col-->
-                        </div>
                         <!-- end row -->
 
                     </div> <!-- end col -->
