ccc46cf505b244d4857f5892e423b7cd5540b107
seba <seig.astorga@gmail.com>
2019-11-22 19:21:31 -0300

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,59 +8,23 @@
                         <div class="card">
 
                             <!-- Logo -->
-<<<<<<< HEAD
-                            <div class="card-header pt-4 pb-4 text-center text-light bg-primary">
-                            
-                                <a href="index.html">
-                                    <span><img src="{{asset ('images/logo.png')}}" alt="..." height="100%" class="rounded-circle"> </span>
-                                </a>
-=======
                             <div class="card-header pt-4 pb-4 text-center bg-primary">
                            
                                     <span><img src="{{asset ('images/logo2.png')}}" alt="" height="75"> <!--<H3 style="color:#FFFFFF";> Inicio de Sesión <H3> --> </span>
                             
->>>>>>> seba
                             </div>
 
                             <div class="card-body p-4">
                                 
                                 <div class="text-center w-75 m-auto">
-<<<<<<< HEAD
-                                    <h4 class="text-dark-50 text-center mt-0 font-weight-bold">Ingresa a OmedSys</h4>
-                                    <p class="text-muted mb-4">Ingresa tu email y contraseña para acceder al panel de administraciòn</p>
-=======
                                     <h4 class="text-dark-50 text-center mt-0 font-weight-bold">INICIA SESIÓN</h4>
                                     <p class="text-muted mb-4">Ingresa tú dirección de correo electrónico y contraseña para acceder al panel de administrador.</p>
->>>>>>> seba
                                 </div>
                                 
                                 <form method="POST" action="{{ route('login') }}">
                                 @csrf
 
                                     <div class="form-group">
-<<<<<<< HEAD
-                                        <label for="emailaddress">{{ __('Direccion de E-mail') }}</label>
-                                        <input id="email" type="email" placeholder="Ingresa tu email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                        @if ($errors->has('email'))
-                                            <span class="invalid-feedback">
-                                                <strong>{{ $errors->first('email') }}</strong>
-                                            </span>
-                                        @endif
-                                    </div>
-
-                                    <div class="form-group">
-                                        <a href="{{ route('password.request') }}" class="text-muted float-right"><small>{{ __('¿Olvidaste tu contraseña?') }}</small></a>
-                                        <label for="password">{{ __('Contraseña') }}</label>
-                                        <input id="password" type="password"  placeholder="Ingresa tu contraseña" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-
-                                         @if ($errors->has('password'))
-                                            <span class="invalid-feedback">
-                                                <strong>{{ $errors->first('password') }}</strong>
-                                            </span>
-                                        @endif
-=======
                                         <label for="emailaddress">Dirección de Correo</label>
                                         <input class="form-control" type="email" id="emailaddress" required="" placeholder="Ingresa tu correo electrónico">
                                     </div>
@@ -69,30 +33,16 @@
                                         <a href="pages-recoverpw.html" class="text-muted float-right"><small>Olvidaste tú contraseña?</small></a>
                                         <label for="password">Contraseña</label>
                                         <input class="form-control" type="password" required="" id="password" placeholder="Ingresa tu contraseña">
->>>>>>> seba
                                     </div>
 
                                     <div class="form-group mb-3">
                                         <div class="custom-control custom-checkbox">
-<<<<<<< HEAD
-                                        <input type="checkbox" name="remember"  class="custom-control-input" id="checkbox-signin"  {{ old('remember') ? 'checked' : '' }}> 
-                                            <label class="custom-control-label" for="checkbox-signin">{{ __('Recuerdame') }}</label>
-=======
                                             <input type="checkbox" class="custom-control-input" id="checkbox-signin" checked="">
                                             <label class="custom-control-label" for="checkbox-signin">Recuerdame</label>
->>>>>>> seba
                                         </div>
                                     </div>
 
                                     <div class="form-group mb-0 text-center">
-<<<<<<< HEAD
-                                        <button class="btn btn-primary" type="submit"> <i class="fas fa-user"></i>
-                                    {{ __('Login') }}</button>
-                                    </div>
-
-                                </form>
-                   <!-- <form method="POST" action="{{ route('login') }}">
-=======
                                         <button class="btn btn-primary" type="submit"> Ingresar </button>
                                     </div>
 
@@ -122,7 +72,6 @@
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
->>>>>>> seba
                         @csrf
 
                         <div class="form-group row">
@@ -178,19 +127,12 @@
                             </div>
                         </div>
                         
-                    </form>-->
-
-
-
-                            </div> <!-- end card-body -->
-                        </div>
-                        <!-- end card -->
-
-
-                    </div> <!-- end col -->
+                    </form>
                 </div>
-                <!-- end row -->
             </div>
+        </div>
+    </div>
+</div> -->
 @endsection
 
 
