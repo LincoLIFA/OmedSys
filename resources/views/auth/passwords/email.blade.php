1484651a1d01c6cdee7f8dcf2f0f749e83cb097b
seba <seig.astorga@gmail.com>
2019-11-22 22:22:24 -0300

    seba

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -6,47 +6,59 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-5">
                         <div class="card">
-                            <!-- Logo -->
+
                             <div class="card-header pt-4 pb-4 text-center bg-primary">
                                 <a href="index.html">
-                                <span><img src="{{asset ('images/logo2.png')}}" alt="" height="75"> <!--<H3 style="color:#FFFFFF";> Inicio de Sesión <H3> --> </span>
+                                <span><img src="{{asset ('images/logo2.png')}}" alt="" height="75"></span>
                                 </a>
                             </div>
                             
                             <div class="card-body p-4">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                                </div>
+                             @endif
+
+                             <form method="POST" action="{{ route('password.email') }}">
+                                    @csrf
                                 
                                 <div class="text-center w-75 m-auto">
-                                    <h4 class="text-dark-50 text-center mt-0 font-weight-bold">RESTABLECER CONTRASEÑA</h4>
+                                    <h4 class="text-dark-50 text-center mt-0 font-weight-bold">{{ __('RESTABLECER CONTRASEÑA') }}</h4>
                                     <p class="text-muted mb-4">Ingrese su dirección de correo electrónico y le enviaremos un mensaje con instrucciones para restablecer su contraseña.</p>
                                 </div>
-
-                                <form action="#">
+                                
                                     <div class="form-group mb-3">
-                                        <label for="emailaddress">Correo electrónico</label>
-                                        <input class="form-control" type="email" id="emailaddress" required="" placeholder="Ingrsa tu correo electrónico">
+                                        <label for="emailaddress">{{ __('Correo electrónico') }}</label>
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                     </div>
 
                                     <div class="form-group mb-0 text-center">
-                                        <button class="btn btn-primary" type="submit">Restablecer Contraseña</button>
+                                        <button class="btn btn-primary" type="submit">{{ __('Restablecer Contraseña') }}</button>
                                     </div>
                                 </form>
-                            </div> <!-- end card-body-->
+                            </div>  
                         </div>
-                        <!-- end card -->
+
 
                         <div class="row mt-3">
                             <div class="col-12 text-center">
-                                <p class="text-muted">Regresa a <a href="pages-login.html" class="text-muted ml-1"><b>Inicio de sesión</b></a></p>
-                            </div> <!-- end col -->
+                                <p class="text-muted">Regresa a <a href="{{route('login')}}" class="text-muted ml-1"><b>Inicio de sesión</b></a></p>
+                            </div> 
                         </div>
-                        <!-- end row -->
-
-                    </div> <!-- end col -->
+                    </div> 
                 </div>
-                <!-- end row -->
-            </div>
+            </div> 
 
-<!--<div class="container">
+
+<!--
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card text-white bg-dark">
@@ -89,4 +101,5 @@
         </div>
     </div>
 </div> -->
+
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,6 +4,63 @@
 
 
 <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5">
+                        <div class="card">
+
+                            <div class="card-header pt-4 pb-4 text-center bg-primary">
+                                <a href="index.html">
+                                <span><img src="{{asset ('images/logo2.png')}}" alt="" height="75"></span>
+                                </a>
+                            </div>
+                            
+                            <div class="card-body p-4">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                                </div>
+                             @endif
+
+                             <form method="POST" action="{{ route('password.update') }}">
+                                    @csrf
+
+                            <div class="text-center w-75 m-auto">
+                                    <h4 class="text-dark-50 text-center mt-0 font-weight-bold">{{ __('CAMBIAR CONTRASEÑA') }}</h4>
+                                    <p class="text-muted mb-4">Ingrese una nueva contraseña para poder iniciar sesión.</p>
+                            </div>
+
+                            <div class="form-group">
+                            <label for="password">{{ __('Nueva Contraseña') }}</label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Ingresa una contraseña" name="password" required autocomplete="new-password">
+
+                                @error('Contraseña')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm">{{ __('Confirma Nueva Contraseña') }}</label>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Repite la contraseña" required autocomplete="new-password">
+                            </div>
+
+                        <div class="form-group text-center mb-0">
+                                <button type="submit" class="btn btn-success">
+                                    <i class="fas fa-user-plus"></i>
+                                    {{ __('Actualizar Contraseña') }}
+                                </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+<!--
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card bg-dark text-white">
@@ -63,5 +120,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
