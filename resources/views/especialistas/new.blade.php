5ac2e359a8ec68131e539f0f5cd80b958a58f940
seba <seig.astorga@gmail.com>
2019-12-04 10:10:13 -0300

    estructura tratamientos

--- resources/views/especialistas/new.blade.php
+++ resources/views/especialistas/new.blade.php
@@ -7,16 +7,21 @@
 
                         
                         <!-- start page title -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box">
-                                    <div class="page-title-right">
-                                       Ingreso de Especialistas
-                                    </div>
-                                    <h4 class="page-title">Gestion de Especialistas</h4>
-                                </div>
-                            </div>
-                        </div>     
+                        <section class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Ingreso de especialistas </h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item active">Advanced Form</li>
+        </ol>
+      </div>
+    </div>
+  </div><!-- /.container-fluid -->
+</section> 
                         <!-- end page title --> 
 
                         <div class="row">
--- resources/views/especialistas/registro.blade.php
+++ resources/views/especialistas/registro.blade.php
@@ -5,14 +5,21 @@
                    
                          <div class="container-fluid">
                         
-                        <!-- start page title -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box">
-                                    <h4 class="page-title">Gestion de Especialistas</h4>
-                                </div>
-                            </div>
-                        </div>     
+                         <section class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Gestión de especialistas </h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item active">Advanced Form</li>
+        </ol>
+      </div>
+    </div>
+  </div><!-- /.container-fluid -->
+</section>
                         <!-- end page title -->
                         
                          <!-- Main content -->
