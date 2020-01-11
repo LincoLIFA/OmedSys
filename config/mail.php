67fabf2c6fee2b44ca1fd21c8bedb7eaa10cba99
LincoLifa <54637459+LincoLIFA@users.noreply.github.com>
2020-01-11 01:01:46 -0300

    configuracion de email en Hostinger

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.hostinger.es'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'system@omedgroup.com'),
+        'name' => env('MAIL_FROM_NAME', 'OmedSys'),
     ],
 
     /*
